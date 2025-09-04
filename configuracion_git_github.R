7a9a323163dbc2d9e0ac0738158c560159da0669
BiologistMich <ivann.romero@outlook.com>
2025-09-03 21:56:27 -0600

    Primeros conceptos de diversidad y clasificacion whittacker

---
+++ configuracion_git_github.R
@@ -0,0 +1,7 @@
+library(usethis)
+git_sitrep() # conocer que usuario de github tengo activo
+use_git() # activar el uso de git
+1
+1
+use_github() #crear y dirigirme a repositorio en github
+git_remotes() #conocer la direccion del repositorio
